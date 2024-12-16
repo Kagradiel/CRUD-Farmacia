86f04916e0e72b1a4fe014d3ca2a9f64ad7966e3
Kagradiel <matheusp_torres@hotmail.com>
2024-12-16 10:55:14 -0300

    primeiro commit

---
+++ src/main/java/com/generation/farmasys/FarmasysApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmasys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmasysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmasysApplication.class, args);
+	}
+
+}
